5917982b6faf48a410cfd7e87d8eaabb36b99f14
kemi_mabel <oluwakemi.mabell@gmail.com>
2022-02-14 11:08:13 +0100

    Initial Commit

---
+++ iSpace/iSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpaceApp.swift
+//  iSpace
+//
+//  Created by Oluwakemi Onajinrin on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct iSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
